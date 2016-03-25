c82ed10ecb2041406ad52746a005e4efb5c1b91a
Connor Ford <connor.ford@ucdenver.edu>
2016-03-25 16:10:15 -0600

    Use helper for API calls

--- admin_app.rb
+++ admin_app.rb
@@ -8,33 +8,22 @@ class AdminApp < Wolf::Base
   use Wolf::AuthFilter
 
   get '/' do
-    url = "#{settings.api_base}/accounts/" \
-          "#{settings.canvas_account_id}" \
-          "/sub_accounts?per_page=50"
-
-    response = RestClient.get(url, auth_header)
-    @schools = JSON.parse(response.body)
-
+    @schools = canvas_api(:get, "accounts/#{settings.canvas_account_id}/" \
+                                "sub_accounts?per_page=50")
     slim :index
   end
 
   get '/admins/:school_id' do
-    url = "#{settings.api_base}/accounts/#{params['school_id']}/admins"
-    response = RestClient.get(url, auth_header)
-    @admins = JSON.parse(response)
-
+    @admins = canvas_api(:get, "accounts/#{params['school_id']}/admins")
     slim :_admins, :layout => false
   end
 
   get '/user/:user_id' do
-    url = "#{settings.api_base}/users/#{params['user_id']}/profile"
-    response = RestClient.get(url, auth_header)
-    @email = JSON.parse(response)['primary_email'] || 'N/A'
+    profile = canvas_api(:get, "/users/#{params['user_id']}/profile")
+    @email = profile['primary_email'] || 'N/A'
 
-    url = "#{settings.api_base}/users/#{params['user_id']}/page_views"
-    response = RestClient.get(url, auth_header)
-    timestamps = JSON.parse(response).collect{ |t| t['created_at'] }
-    last = timestamps.reject(&:nil?).sort.last
+    timestamps = canvas_api(:get, "/users/#{params['user_id']}/page_views")
+    last = timestamps.collect{ |t| t['created_at'] }.reject(&:nil?).sort.last
     @last_activity = last ? Time.parse(last).strftime("%B %d, %Y") : 'N/A'
 
     slim :_user, :layout => false
